07707aab4cd0f39017e82a5133e7ab3acf16c32e
sunzequn <sunzequn@live.cn>
2023-08-15 16:52:31 +0800

    embed_sf v1.0

--- README.md
+++ README.md
@@ -2,22 +2,38 @@
 
 > Joint representation learning over multi-sourced knowledge graphs (KGs) yields transferable and expressive embeddings that improve downstream tasks. Entity alignment (EA) is a critical step in this process. Despite recent considerable research progress in embedding-based EA, how it works remains to be explored. In this paper, we provide a similarity flooding perspective to explain existing translation-based and aggregation-based EA models. We prove that the embedding learning process of these models actually seeks a fixpoint of pairwise similarities between entities. We also provide experimental evidence to support our theoretical analysis. We propose two simple but effective methods inspired by the fixpoint computation in similarity flooding, and demonstrate their effectiveness on benchmark datasets. Our work bridges the gap between recent embedding-based models and the conventional similarity flooding algorithm. It would improve our understanding of and increase our faith in embedding-based EA.
 
+*** UPDATE *** 
+
+* Aug. 15, 2023: We improve and update the implementation for *similarity flooding via entity compositions*, which can achieve slightly better results than those reported in our ICML paper.
+
 ## Dependencies
 
-The code for *embedding-based similarity flooding* is dependent on:
+The implementation for *similarity flooding via entity compositions* is dependent on:
 
-* python3 (*tested with v3.10.12*)
-* pandas (*tested with v1.5.3*)
-* numpy (*tested with v1.22.3*)
-* scipy (*tested with v1.7.3*)
-* cudatoolkit (*tested with v10.2.89*)
-* cupy (*tested with v12.1.0*)
+* **python3** (*tested with v3.10.12*)
+* **pandas** (*tested with v1.5.3*)
+* **numpy** (*tested with v1.22.3*)
+* **scipy** (*tested with v1.7.3*)
+* **cudatoolkit** (*tested with v10.2.89*)
+* **cupy** (*tested with v12.1.0*)
 
 ## Running code
 
-First, we need to execute the *preprocess()* function to process the DBP15K dataset.
+### Similarity flooding via entity compositions
 
-Then, for example, to run TransFlood on *ZH-EN*, please execute function *train('zh_en')*.
+To run TransFlood on DBP15K ZH-EN, please execute the following script:
+
+```bash
+python embed_sf.py --input ./dataset/dbp15k/zh_en/ --model TransE
+```
+
+To run GCNFlood on DBP15K ZH-EN, please execute:
+
+```bash
+python embed_sf.py --input ./dataset/dbp15k/zh_en/ --model GCN
+```
+
+> If you have any difficulty or question in running code and reproducing experimental results, please email to zqsun.nju@gmail.com or jchuang.nju@gmail.com.
 
 ## Citation
 If you find the work helpful, please kindly cite the following paper:
--- embed_sf.py
+++ embed_sf.py
@@ -1,4 +1,5 @@
-import pandas as pd
+import argparse
+
 import pandas as pd
 import numpy as np
 from pandas.api.types import CategoricalDtype
@@ -7,175 +8,194 @@ import cupy as cp
 from pathlib import Path
 
 
+parser = argparse.ArgumentParser(description='Embed_SF')
+parser.add_argument('--input', type=str, default="./dataset/dbp15k/zh_en/")
+# parser.add_argument('--input', type=str, default="./dataset/openea/D_W_15K_V1/")
+parser.add_argument('--model', type=str, default="TransE")
+
+
 class KnowledgeGraph():
-    def __init__(self, triples, type_rel) -> None:
+    def __init__(self, triples) -> None:
         super().__init__()
         for col in ['h', 'r', 't']:
             triples[col] = triples[col].astype(str)
         ents = np.sort(pd.concat([triples['h'], triples['t']]).unique())
         self.entities = CategoricalDtype(ents, ordered=True)
-        ht = triples[triples['r'] == type_rel][['h', 't']]
-        classes = np.sort(ht['t'].unique())
-        self.classes = CategoricalDtype(classes, ordered=True)
-        self.triples = triples.drop(index=ht.index).drop_duplicates()
+        self.triples = triples.copy()
         self.triples['h'] = self.triples['h'].astype(self.entities)
         self.triples['t'] = self.triples['t'].astype(self.entities)
-        self.relations = CategoricalDtype(np.sort(self.triples['r'].unique()), ordered=True)
+        self.relations = CategoricalDtype(
+            np.sort(self.triples['r'].unique()), ordered=True)
         self.triples['r'] = self.triples['r'].astype(self.relations)
         assert not self.triples.isna().values.any()
-        # assert not self.type_triples.isna().values.any()
-    
-    def entity_count(self):
-        return len(self.entities.categories)
-    
-    def relation_count(self):
-        return len(self.relations.categories)
-    
-    def class_count(self):
-        return len(self.classes.categories)
-    
-    def __call__(self, triples : pd.DataFrame, ent_cols=None, rel_cols=None, cls_cols=None):
+
+    def __call__(self, triples: pd.DataFrame, ent_cols=None, rel_cols=None):
         triples = triples.copy()
 
-        for col in (['h', 't'] if ent_cols is None else ent_cols):
-            if col in triples.columns:
-                raw = triples[col]
-                triples[col] = triples[col].astype(str).astype(self.entities)
-                if triples[col].isna().any():
-                    print('kg encode error %s' % col)
-                    print(raw[triples[col].isna()])
+        for [presets, cols, dtype] in ([['h', 't'], ent_cols, self.entities], [['r'], rel_cols, self.relations]):
+            for col in (presets if cols is None else cols):
+                if col in triples.columns:
+                    raw = triples[col]
+                    triples[col] = triples[col].astype(str).astype(dtype)
+                    if triples[col].isna().any():
+                        print('kg encode error %s' % col)
+                        print(raw[triples[col].isna()])
 
-        for col in (['r'] if rel_cols is None else rel_cols):
-            if col in triples.columns:
-                raw = triples[col]
-                triples[col] = triples[col].astype(self.relations)
-                if triples[col].isna().any():
-                    print('kg encode error %s' % col)
-                    print(raw[triples[col].isna()])
-        
-        for col in ([] if cls_cols is None else cls_cols):
-            if col in triples.columns:
-                raw = triples[col]
-                triples[col] = triples[col].astype(self.classes)
-                if triples[col].isna().any():
-                    print('kg encode error %s' % col)
-                    print(raw[triples[col].isna()])
-        
         return triples.dropna()
 
 
-def train(task_name, max_step=2):
-    base_dir = Path('./dbp15k/') / task_name / 'mtranse' / '0_3'
+def counting(df, idx_col, val_col, dims):
+    cnt = df.groupby(by=idx_col)[val_col].count().reset_index()
+    dim = (dims[idx_col[0]], dims[idx_col[1]])
+    return ss.csr_matrix((
+        cnt[val_col].values, 
+        (cnt[idx_col[0]].values, cnt[idx_col[1]].values)
+    ), dim)
 
-    train = np.load(base_dir / 'train.npy')
-    test = np.load(base_dir / 'test.npy')
-    # valid = np.load(base_dir / 'valid.npy')
 
-    H1 = ss.load_npz(base_dir / 'kg1.npz').toarray()
-    H2 = ss.load_npz(base_dir / 'kg2.npz').toarray()
-    T = ss.csr_matrix((np.ones(train.shape[0]), (train[:, 0], train[:, 1])), shape=H1.shape).toarray()
+def load_data_dbp15k(task_dir):
+    print("load dbp15k data...")
+    task_dir = task_dir / 'mtranse' / '0_3'
+    kg1, kg2 = [
+        KnowledgeGraph(
+            pd.read_csv(task_dir / part, names=['h', 'r', 't'], sep='\t')
+        ) for part in ['triples_1', 'triples_2']
+    ]
 
-    H1 = H1 / np.maximum(np.sqrt(np.square(H1).sum(1, keepdims=True)), 1e-8)
-    H2 = H2 / (np.sqrt(np.square(H2).sum(1, keepdims=True)) + 1e-8)
-    H1 = H1 / (H1.sum(1, keepdims=True) + 1e-8)
-    H2 = H2 / (H2.sum(1, keepdims=True) + 1e-8)
-    H1 = H1 / np.sqrt(np.square(H1).sum(1, keepdims=True))
-    H2 = H2 / np.sqrt(np.square(H2).sum(1, keepdims=True))
-    H1[np.isnan(H1)] = 0.0
-    H2[np.isnan(H2)] = 0.0
+    train, test = [
+        kg1(kg2(
+            pd.read_csv(task_dir / part, names=['e1', 'e2'], sep='\t'),
+            ent_cols=['e2']),
+            ent_cols=['e1'])
+        for part in ['sup_pairs', 'ref_pairs']
+    ]
 
+    train, test = [
+        pd.DataFrame(dict((k, part[k].cat.codes) for k in ['e1', 'e2'])).values
+        for part in [train, test]
+    ]
+
+    return kg1, kg2, train, test
+
+
+def load_data_openea(task_dir, splition_id):
+    print("load openea data...")
+    kg1, kg2 = [
+        KnowledgeGraph(
+            pd.read_csv(task_dir / part, names=['h', 'r', 't'], sep='\t')
+        ) for part in ['rel_triples_1', 'rel_triples_2']
+    ]
+
+    train, test = [
+        kg1(kg2(
+            pd.read_csv(task_dir / '721_5fold' / str(splition_id) / part, names=['e1', 'e2'], sep='\t'),
+            ent_cols=['e2']),
+            ent_cols=['e1'])
+        for part in ['train_links', 'test_links']
+    ]
+
+    train, test = [
+        pd.DataFrame(dict((k, part[k].cat.codes) for k in ['e1', 'e2'])).values
+        for part in [train, test]
+    ]
+
+    return kg1, kg2, train, test
+
+
+def transe_lambda_weight(triples):
+    print("compute lambda values based on TransE...")
+    ent_dim = max(triples['h'].max(), triples['t'].max()) + 1
+    rel_dim = triples['r'].max() + 1
+    dims = dict(h=ent_dim, t=ent_dim, r=rel_dim)
+    rh = counting(triples, ['r', 'h'], 't', dims)
+    rt = counting(triples, ['r', 't'], 'h', dims)
+    R = (rt - rh).multiply(ss.csr_matrix(1. / np.maximum(rh.sum(1), 1)))
+
+    et = counting(triples, ['h', 't'], 'r', dims)
+    eh = counting(triples, ['t', 'h'], 'r', dims)
+    e_c = et.sum(1) + eh.sum(1)
+    e = eh + et
+
+    et = -counting(triples, ['h', 'r'], 't', dims) @ R
+    eh = counting(triples, ['t', 'r'], 'h', dims) @ R
+    e += et + eh
+
+    return e.multiply(ss.csr_matrix(1. / np.maximum(e_c, 1))), (e, e_c)
+
+
+def gcn_lambda_weight(triples):
+    print("compute lambda values based on GCN...")
+    ent_dim = max(triples['h'].max(), triples['t'].max()) + 1
+    ent_dim = max(triples['h'].max(), triples['t'].max()) + 1
+    rel_dim = triples['r'].max() + 1
+    dims = dict(h=ent_dim, t=ent_dim, r=rel_dim)
+
+    et = (counting(triples, ['h', 't'], 'r', dims) >= 1).astype(int)
+    eh = (counting(triples, ['t', 'h'], 'r', dims) >= 1).astype(int)
+    e_c = et.sum(1) + eh.sum(1)
+    e = eh + et
+
+    return e.multiply(1. / e_c), (e, e_c)
+
+
+def run_train(kg1, kg2, train, test, initial_sim=None, lambda_fn=transe_lambda_weight, max_step=3):
+
+    t1, t2 = (pd.DataFrame(dict((col, kg.triples[col].cat.codes) for col in 'hrt')) for kg in (kg1, kg2))
+
+    H1, _ = lambda_fn(t1)
+    H2, _ = lambda_fn(t2)
+    T = ss.csr_matrix((np.ones(train.shape[0]), (train[:, 0], train[:, 1])), shape=(H1.shape[0], H2.shape[0]))
+    H1 = H1.toarray()
+    H2 = H2.toarray()
+    H1 = cp.array(H1 / np.sqrt(np.square(H1).sum(1, keepdims=True)))
+    H2 = cp.array(H2 / np.sqrt(np.square(H2).sum(1, keepdims=True)))
+    T = T.toarray()
+
+    T = cp.array(T)
     T1 = T.copy()
     T1 = np.clip(T, 0, 1)
-    for i in range(0, 2):
-        T1 = cp.asnumpy(cp.matmul(cp.matmul(cp.array(H1), cp.array(T1)), cp.array(H2.T)))
-        cp.get_default_memory_pool().free_all_blocks()
-        
-        T1 = np.clip(T1 + T * 2, 0, 1)
+    for i in range(0, max_step):
+        T1 = cp.array(T1)
+        T1 = cp.matmul(cp.matmul(H1, T1), H2.T)
 
-        T1 = T1 / (np.maximum((T1 * (T1 > 0)).sum(1, keepdims=True), 1))
-        T1 = T1 / (np.maximum((T1 * (T1 > 0)).sum(0, keepdims=True), 1))
+        T1 = cp.clip(T1 + T * 2, -1, 1)
+        # csls
+        T1 = T1 - T1.mean(1, keepdims=True) * 0.5 - T1.mean(0, keepdims=True) * 0.5
+        T1 = cp.clip(T1 + T * 2, -1, 1)
 
-        # T1 = T1 - T1.mean(1, keepdims=True) * 0.5 - T1.mean(0, keepdims=True) * 0.5
-        T1 = np.clip(T1 + T * 2, 0, 1)
+        # add penalty to duplicated matches of each entity
+        scale = cp.maximum(
+            (T1 * (T1 > 0.0)).sum(1, keepdims=True),
+            (T1 * (T1 > 0.0)).sum(0, keepdims=True)
+        )
+
+        T1 = T1 / cp.maximum(scale, 1)
+
+
+        T1 = cp.clip(T1 + T * 2, -1, 1)
+        T1 = cp.asnumpy(T1)
+
+        if initial_sim is not None:
+            T1 = T1 * 0.9 + initial_sim* 0.1
+
         out = T1[np.ix_(test[:, 0], test[:, 1])]
+        # evaluation
         rank = (out >= np.diag(out).reshape((-1, 1))).sum(1)
-        print("H@1/5/10 and MR = ", (rank <= 1).mean(), (rank <= 5).mean(), (rank <= 10).mean(), rank.mean())
-
-def preprocess():
-    base_dir = Path('./dbp15k/')
-    dim = 20000
-    for task_name in ['fr_en', 'ja_en', 'zh_en']:
-        print(task_name)
-        task_dir = base_dir / task_name / 'mtranse' / '0_3'
-        kg1 = KnowledgeGraph(pd.read_csv(task_dir / 'triples_1', names=['h', 'r', 't'], sep='\t'), type_rel='ttt')
-        kg2 = KnowledgeGraph(pd.read_csv(task_dir / 'triples_2', names=['h', 'r', 't'], sep='\t'), type_rel='ttt')
-
-        train, test = [
-            kg1(kg2(
-                pd.read_csv(task_dir / part, names=['e1', 'e2'], sep='\t'), 
-                ent_cols=['e2']),
-            ent_cols=['e1'])
-            for part in ['sup_pairs', 'ref_pairs']
-        ]
-
-        np.save(task_dir / 'train', pd.DataFrame(dict(e1=train['e1'].cat.codes, e2=train['e2'].cat.codes)).values)
-        np.save(task_dir / 'test', pd.DataFrame(dict(e1=test['e1'].cat.codes, e2=test['e2'].cat.codes)).values)
-
-        for name, kg in (('kg1', kg1), ('kg2', kg2)):
-            r_vecs = {}
-            for r, g in kg.triples.assign(r=kg.triples['r'].cat.codes).groupby(by='r'):
-                gl = g.shape[0]
-                vec = np.zeros(dim)
-                for t in g['t'].cat.codes:
-                    vec[t] += 1
-                for t in g['h'].cat.codes:
-                    vec[t] -= 1
-                idx = np.arange(dim)[vec != 0]
-                vec = vec / gl
-
-                r_vecs[r] = ss.csr_matrix((vec[idx], (np.zeros(len(idx)), idx)), shape=(1, dim))
-            print(len(r_vecs))
-
-            h_vecs = {}
-            h_cnt = {}
-            for h, g in kg.triples.assign(h=kg.triples['h'].cat.codes).groupby(by='h'):
-                gl = g.shape[0]
-                t_cnt = {}
-                for t in g['t'].cat.codes:
-                    t_cnt[t] = t_cnt.get(t, 0) + 1
-                k = list(t_cnt.keys())
-                v = [t_cnt[_] for _ in k]
-                vec = ss.csr_matrix((v, (np.zeros(len(k)), k)), shape=(1, dim))
-                for r in g['r'].cat.codes:
-                    vec -= r_vecs[r]
-                h_vecs[h] = vec
-                h_cnt[h] = gl
-            
-            for t, g in kg.triples.assign(t=kg.triples['t'].cat.codes).groupby(by='t'):
-                gl = g.shape[0]
-                t_cnt = {}
-                for h in g['h'].cat.codes:
-                    t_cnt[h] = t_cnt.get(h, 0) + 1
-                k = list(t_cnt.keys())
-                v = [t_cnt[_] for _ in k]
-                vec = ss.csr_matrix((v, (np.zeros(len(k)), k)), shape=(1, dim))
-                for r in g['r'].cat.codes:
-                    vec += r_vecs[r]
-                if t in h_vecs:
-                    h_vecs[t] += vec
-                else:
-                    h_vecs[t] = vec
-                    h_cnt[t] = 0
-                h_vecs[t] = h_vecs[t] / (h_cnt[t] + gl)
-
-            h_vecs_k = list(h_vecs.keys())
-            data = np.concatenate([h_vecs[i].data for i in h_vecs_k])
-            x = np.concatenate([[i] * h_vecs[i].data.shape[0] for i in h_vecs_k])
-            y = np.concatenate([h_vecs[i].indices for i in h_vecs_k])
-            H = ss.csr_matrix((data, (x, y)), shape=(dim, dim))
-            ss.save_npz(task_dir / (name + '.npz'), H)
+        print("epoch {}: H@1={}, H@10={}, MRR={}".format(i+1, (rank <= 1).mean(), (rank <= 10).mean(), (1. / rank).mean()))
+        # yield (rank <= 1).mean(), (rank <= 10).mean(), (1. / rank).mean()
 
 
 if __name__ == '__main__':
-    # preprocess()
-    train('zh_en')
+    args = parser.parse_args()
+
+    if "openea" in args.input:
+        kg1, kg2, train, test = load_data_openea(Path(args.input), 3)
+    elif "dbp15k" in args.input:
+        kg1, kg2, train, test = load_data_dbp15k(Path(args.input))
+    
+    if args.model == "TransE":
+        lambda_fn = transe_lambda_weight
+    elif args.model == "GCN":
+        lambda_fn = gcn_lambda_weight
+    
+    run_train(kg1, kg2, train, test, initial_sim=None, lambda_fn=lambda_fn, max_step=5)
